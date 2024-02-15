8571b38660e81bfd80fd2e873dc7e193e385285d
colinguinane1 <colinguinane@gmail.com>
2024-02-15 11:17:12 -0700

    added button that links to github

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var navbar = document.getElementById("navbar")
+var navbarText = document.getElementsByClassName("navbar_text")
 
 window.addEventListener("scroll", function Scroll(){
     navbar.classList.toggle("scrolled", window.scrollY > 0);
+    navbarText.classList.toggle("scrolledA", window.scrollY > 0);
 })
+
