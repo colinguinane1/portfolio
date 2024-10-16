54ffd7c69e35340430f412f2f1491c2e410c084d
colinguinane1 <colinguinane@gmail.com>
2024-10-16 01:10:43 -0600

    dm_sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "../components/Header";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { DM_Sans } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const dm_sans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,10 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={` ${dm_sans.className} antialiased`}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
           <div>{children}</div>
