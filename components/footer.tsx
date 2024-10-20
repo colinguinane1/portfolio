aed693be734d959144b4eb8e991fb9a21d7989b6
colinguinane1 <colinguinane@gmail.com>
2024-10-20 00:34:07 -0600

    refactor: Adjust visibility threshold for FadeInView component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="py-4">
       <div className="container mx-auto px-6 text-center">
         <p>&copy; 2023 Colin Guinane. All rights reserved.</p>
+        <Link className="font-extrabold" href="mailto:colin@c-g.dev">
+          colin@c-g.dev
+        </Link>
       </div>
     </footer>
   );
