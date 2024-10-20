ce1b4581a0334b188af2efe4535214696bc356d6
colinguinane1 <colinguinane@gmail.com>
2024-10-19 22:42:48 -0600

    added post counter

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,14 +5,15 @@ export default async function Blogs() {
   const posts = await getAllPosts();
 
   return (
-    <section className="w-screen">
+    <section className="">
       <div className="flex flex-col gap-8 p-4 max-w-3xl z-10 w-full  justify-between">
         <div>
           <h2 className="text-5xl text-left sm:text-6xl font-black">Blog</h2>
         </div>
-        <div className="text-lg">
+        <div className="">
           <p>I occasionally write about something that interests me.</p>
         </div>
+        <p>{posts.length} posts so far.</p>
 
         <div className="w-full">
           <div className="space-y-4">
